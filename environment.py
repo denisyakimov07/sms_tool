d3337b961f9a9d4f1e6ee1f708c486f5bfb79f09
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-11-13 23:07:14 -0500

    v 0.02

---
+++ environment.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+load_dotenv(verbose=True)
+
+
+class _Environment:
+
+    def __init__(self):
+
+        '''DB_PASSWORD'''
+        self.DB_DATABASE_TYPE = os.getenv('DB_DATABASE_TYPE')
+        self.DB_HOST = os.getenv('DB_HOST')
+        self.DB_USER = os.getenv('DB_USER')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD')
+        self.DB_DATABASE = os.getenv('DB_DATABASE')
+
+        '''API_PASSWORD'''
+        self.ACU_USER_ID = os.getenv('USER_ID')
+        self.ACU_API_KEY = os.getenv('API_KEY')
+
+
+__environment = _Environment()
+
+
+def get_env():
+    return __environment
