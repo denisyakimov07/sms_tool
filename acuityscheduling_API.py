dad36b6123b0492cfa86055d42f1e10ecab9a88e
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-11-14 15:46:08 -0500

    add logger

--- acuityscheduling_API.py
+++ acuityscheduling_API.py
@@ -1,10 +1,10 @@
-
+import logging
 
 
 import requests
 from requests.auth import  HTTPBasicAuth
 
-
+db_logging = logging.getLogger('db')
 
 
 from environment import get_env
@@ -13,13 +13,20 @@ from sms.models import CustomerAPI
 
 API_ENDPOINT = 'https://acuityscheduling.com/api/v1/appointments'
 
-
-
+db_logging = logging.getLogger('db')
 
 def api_get_appointments(min_date=None, max_date=None) -> list[CustomerAPI]:
-    body = {"minDate": min_date, "maxDate": max_date, "excludeForms": False}
-    response = requests.get(API_ENDPOINT, params=body, auth=HTTPBasicAuth(get_env().ACU_USER_ID, get_env().ACU_API_KEY))
-    return [CustomerAPI(**i) for i in response.json()]
+    try:
+        body = {"minDate": min_date, "maxDate": max_date, "excludeForms": False}
+        response = requests.get(API_ENDPOINT, params=body, auth=HTTPBasicAuth(get_env().ACU_USER_ID, get_env().ACU_API_KEY))
+        cus_list = [CustomerAPI(**i) for i in response.json()]
+
+        db_logging.info(f"Get customers from API, total - {len(cus_list)}")
+        return cus_list
+    except Exception as e:
+        db_logging.error(e)
+
+
 
 
 
