54f83d5a90746155fd725c5da56521ae3c9328f3
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-11-17 12:45:06 -0500

    setup twilio

--- sms/views.py
+++ sms/views.py
@@ -78,6 +78,6 @@ def update_appointments_for_two_last_days():
 def read_sms_from_customer(request):
     if request.method == 'POST':
         print(request.POST)
-        payload = json.loads(request.body)
+        payload = json.loads(request.POST)
         print(payload)
         return JsonResponse({'test':1}, safe=False)
