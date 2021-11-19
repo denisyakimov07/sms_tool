64d10e0ccc6a147fb769de6c3835185b50541010
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-11-19 17:21:34 -0500

    v 0.1

--- sms/scheduler/scheduler.py
+++ sms/scheduler/scheduler.py
@@ -19,6 +19,6 @@ def start():
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
     scheduler.add_job(update_app, trigger='cron', hour="14", minute='23', id="Update_acuityscheduling_appointments" , replace_existing=True)
-    scheduler.add_job(daily_report, trigger='cron', hour="17", minute='21', id="Sent_daily_report" , replace_existing=True)
+    scheduler.add_job(daily_report, trigger='cron', hour="17", minute='25', id="Sent_daily_report" , replace_existing=True)
     scheduler.add_job(sent_sms, trigger='cron', hour="14", minute='25', id="Sent_sms", replace_existing=True)
     scheduler.start()
