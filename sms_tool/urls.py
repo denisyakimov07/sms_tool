e2e6f44b9af43bb24b1fac1c70354f26b8369d26
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-12-12 09:50:07 -0500

    added- zendesk funck

--- sms_tool/urls.py
+++ sms_tool/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path
 
 from sms import views
-from sms.models import Customer
-from sms.views import feedback_sms_sender
-from sms.zendesk_api import sms_processor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +10,3 @@ urlpatterns = [
     path('sms/api/v1/webhook/zendesk', views.zendesk_webhook),
     path('', views.redirect_view),
 ]
-
-# sms_processor(new_customer = Customer.objects.filter(phone_number__contains=8312168131)[0], sms_text= "Test dsadsaads111")
